d79f5b62b38dfdaf0711ffd9886ad9096348b0e3
mozumderTushar <itztushar55@gmail.com>
2021-02-01 12:45:32 +0600

    db connected

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jos17.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const port = 5000
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const professionalAppointmentCollection = client.db("mentex").collection("professionalAppointments");
+  console.log('db connected');
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to MenTex')
+})
+
+app.listen(process.env.PORT || port)
